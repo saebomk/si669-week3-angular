aa7ddb86f75d79a34466c0e0d6fa9ce1c243b1ec
saebomk <saebom@umich.edu>
2018-09-25 19:27:54 -0400

    Img

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,7 +1,7 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice', address: "/Users/kwonSaebom/Downloads/architecture-building-culture-380707.jpg" },
+  { id: 11, name: 'Mr. Nice', address: "https://images.pexels.com/photos/262897/pexels-photo-262897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" },
   { id: 12, name: 'Narco', address: "https://images.pexels.com/photos/380707/pexels-photo-380707.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" },
   { id: 13, name: 'Bombasto', address: "https://images.pexels.com/photos/52547/lantern-festival-seoul-cheonggyecheon-stream-lantern-52547.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" },
   { id: 14, name: 'Celeritas', address: "https://images.pexels.com/photos/366107/pexels-photo-366107.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" },

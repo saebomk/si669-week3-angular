ca798ca52e10a81de83e964f29da67602ce5f6e2
saebomk <saebom@umich.edu>
2018-09-25 19:21:41 -0400

    Images

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Mr. Nice', address: "/Users/kwonSaebom/Downloads/architecture-building-culture-380707.jpg" },
+  { id: 12, name: 'Narco', address: "https://images.pexels.com/photos/380707/pexels-photo-380707.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" },
+  { id: 13, name: 'Bombasto', address: "https://images.pexels.com/photos/52547/lantern-festival-seoul-cheonggyecheon-stream-lantern-52547.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" },
+  { id: 14, name: 'Celeritas', address: "https://images.pexels.com/photos/366107/pexels-photo-366107.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" },
+  { id: 15, name: 'Magneta', address: "https://images.pexels.com/photos/1368534/pexels-photo-1368534.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" },
+  { id: 16, name: 'RubberMan', address: "https://images.pexels.com/photos/424759/pexels-photo-424759.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" },
+  { id: 17, name: 'Dynama', address: "https://images.pexels.com/photos/1119723/pexels-photo-1119723.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" },
+  { id: 18, name: 'Dr IQ', address: "https://images.pexels.com/photos/53350/seoul-skyscraper-building-architecture-53350.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" },
+  { id: 19, name: 'Magma', address: "https://images.pexels.com/photos/380710/pexels-photo-380710.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" },
+  { id: 20, name: 'Tornado', address: "https://images.pexels.com/photos/290604/pexels-photo-290604.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" }
 ];
